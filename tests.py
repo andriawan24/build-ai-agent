bc5f2bf93051950e6f41f8b3afbcf71e69df3852
andriawan24 <fawaznaufal23@gmail.com>
2025-06-13 10:07:15 +0700

    feature: Remove lorem and update tests

--- tests.py
+++ tests.py
@@ -1,35 +1,34 @@
 # import unittest
 from functions.run_python import run_python
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+import unittest
 
-# class TestFunction(unittest.TestCase):
-#     def test_print_root_folder(self):
-#         result = get_files_info("calculator", ".")
-#         self.assertTrue("main.py" in result)
-#         self.assertTrue("tests.py" in result)
-#         self.assertTrue("pkg" in result)
-#         self.assertTrue("render.py" not in result)
+class TestFunction(unittest.TestCase):
+    def test_print_root_folder(self):
+        result = get_files_info("calculator", ".")
+        self.assertTrue("main.py" in result)
+        self.assertTrue("tests.py" in result)
+        self.assertTrue("pkg" in result)
+        self.assertTrue("render.py" not in result)
 
-#     def test_print_subfolder(self):
-#         result = get_files_info("calculator", "pkg")
-#         self.assertTrue("main.py" not in result)
-#         self.assertTrue("calculator.py" in result)
-#         self.assertTrue("render.py" in result)
+    def test_print_subfolder(self):
+        result = get_files_info("calculator", "pkg")
+        self.assertTrue("main.py" not in result)
+        self.assertTrue("calculator.py" in result)
+        self.assertTrue("render.py" in result)
 
-#     def test_print_invalid_folder(self):
-#         with self.assertRaises(Exception) as context:
-#             get_files_info("calculator", "/bin")
+    def test_print_invalid_folder(self):
+        with self.assertRaises(Exception) as context:
+            get_files_info("calculator", "/bin")
 
-#         self.assertTrue('outside the permitted working directory' in str(context.exception))
+        self.assertTrue('outside the permitted working directory' in str(context.exception))
 
-#     def test_print_another_invalid_folder(self):
-#         with self.assertRaises(Exception) as context:
-#             get_files_info("calculator", "../")
+    def test_print_another_invalid_folder(self):
+        with self.assertRaises(Exception) as context:
+            get_files_info("calculator", "../")
 
-#         self.assertTrue('outside the permitted working directory' in str(context.exception))
+        self.assertTrue('outside the permitted working directory' in str(context.exception))
 
 if __name__ == "__main__":
-    print(run_python("calculator", "main.py"))
-    print(run_python("calculator", "tests.py"))
-    print(run_python("calculator", "../main.py"))
-    print(run_python("calculator", "nonexistent.py"))
-    # unittest.main()
+    unittest.main()
